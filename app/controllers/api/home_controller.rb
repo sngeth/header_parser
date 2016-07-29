01970b61d7a0a91948b3fb3f88128988fabc7077
Sid Ngeth <sngeth@gmail.com>
2016-07-29 08:46:39 -0400

    Add user info to json response

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -1,5 +1,21 @@
 class Api::HomeController < ApplicationController
   def index
-    render json: {status: :success}.as_json
+    render json: {
+    ip_address: ip_address,
+    language: language,
+    software: software}.as_json
+  end
+
+  private
+  def ip_address
+    request.remote_ip
+  end
+
+  def language
+    request.env['HTTP_ACCEPT_LANGUAGE']
+  end
+
+  def software
+    request.env['HTTP_USER_AGENT']
   end
 end
